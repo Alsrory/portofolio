ea917a85057ddd1c177687063e432fc5ece9cce9
Alsrory <wwwalsarory55@gmail.comm>
2025-08-04 18:07:25 +0300

    add head

--- app/head.tsx
+++ app/head.tsx
@@ -5,8 +5,10 @@ export default function Head() {
       <meta name="description" content="أنا وجدي السروري، مطوّر Laravel وReact بخبرة في تطوير تطبيقات الويب الحديثة. Explore modern full-stack projects using Laravel, Next.js, and REST APIs." />
       <meta name="google-site-verification" content="google778d2074ea81e0a6" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <mata name="me"  content="mmmmmmmmm"/>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
+      
     </>
   );
 }
